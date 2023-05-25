836baa0c140b8829ff22cb4168a5027c8d066bdc
Weston Schutt <wtschutt@gmail.com>
2023-05-25 16:34:16 -0600

    Test: add tests for .place_ship and .fired_upon methods

--- spec/cell_spec.rb
+++ spec/cell_spec.rb
@@ -3,18 +3,36 @@ require 'spec_helper'
 Rspec.describe Cell do
   
   before(:each) do
-    cell = Cell.new('B4')
+    @cell = Cell.new('B4')
   end
 
   it 'exists' do
     expect(cell).to be_an_instance_of(Cell)
   end
 
-  it 'checks if cell has object' do
+  it 'checks if cell has object instances' do
     expect(cell.coordinate).to eq('B4') 
     expect(cell.ship).to eq(nil)
     expect(cell.empty?).to eq(true)
   end
 
-  # end test
+  it 'can place a ship on the cell' do
+    @cell.place_ship(@cruiser)
+    expect(@cell.ship).to eq(@cruiser)
+    expect(@cell.empty?).to eq(false)
+  end
+
+  it 'is not initially fired upon' do
+    @cell.place_ship(@cruiser)
+    expect(@cell.fired_upon?).to eq(false)
+  end
+
+  it 'knows when it has been fired upon' do
+    @cell.place_ship(@cruiser)
+    
+    @cell.fired_upon
+    
+    expect(@cell.ship.health).to eq(2)
+    expect(@cell.fired_upon?).to eq(true)
+  end
 end
