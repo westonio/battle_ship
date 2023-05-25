c5fa779f4aeccb2ac2b5ed8bf3bbca62c03ceb61
Weston Schutt <wtschutt@gmail.com>
2023-05-25 16:49:45 -0600

    Test: Add rendering tests

--- spec/cell_spec.rb
+++ spec/cell_spec.rb
@@ -4,6 +4,7 @@ Rspec.describe Cell do
   
   before(:each) do
     @cell = Cell.new('B4')
+    @cruiser = Ship.new("Cruiser", 3)
   end
 
   it 'exists' do
@@ -30,9 +31,47 @@ Rspec.describe Cell do
   it 'knows when it has been fired upon' do
     @cell.place_ship(@cruiser)
     
-    @cell.fired_upon
+    @cell.fire_upon
     
     expect(@cell.ship.health).to eq(2)
     expect(@cell.fired_upon?).to eq(true)
+  end
+
+  describe 'renterings' do
+    it 'renders ”.” if the cell has not been fired upon' do
+      expect(@cell.render).to eq(".")
+    end
+  
+    it 'renders “M” if the cell has been fired upon and it does not contain a ship (the shot was a miss)' do
+      @cell.fire_upon
+  
+      expect(@cell.fired_upon?).to eq(true)
+      expect(@cell.render).to eq("M")
+    end
+  
+    it 'renders “H” if the cell has been fired upon and it contains a ship (the shot was a hit)' do
+      @cell.place_ship(@cruiser)
+      @cell.fire_upon
+
+      expect(@cell.fired_upon?).to eq(true)
+      expect(@cell.render).to eq("H")
+    end
+  
+    it 'renders “X” if the cell has been fired upon and its ship has been sunk.' do
+      @cell.place_ship(@cruiser)
+      @cell.fire_upon
+      @cruiser.hit
+      @cruiser.hit
+      
+      expect(@cell.ship.sunk?).to eq(true)
+      expect(@cell.render).to eq("X")
+    end
+  
+    it 'can optionally render ship not found yet' do
+      expect(@cell.render).to eq(".")
+      @cell.place_ship(@cruiser)
+      
+      expect(@cell.render(true)).to eq("S")
+    end
   end
 end
