9606c5428e8dba20ec04243a9bb1d0fa2a4c10e2
Connor Richmond <richmond.connor@gmail.com>
2023-05-25 19:38:08 -0400

    Replace 'cell' var with @cell to call properly

--- spec/cell_spec.rb
+++ spec/cell_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-Rspec.describe Cell do
+RSpec.describe Cell do
   
   before(:each) do
     @cell = Cell.new('B4')
@@ -8,13 +8,13 @@ Rspec.describe Cell do
   end
 
   it 'exists' do
-    expect(cell).to be_an_instance_of(Cell)
+    expect(@cell).to be_an_instance_of(Cell)
   end
 
   it 'checks if cell has object instances' do
-    expect(cell.coordinate).to eq('B4') 
-    expect(cell.ship).to eq(nil)
-    expect(cell.empty?).to eq(true)
+    expect(@cell.coordinate).to eq('B4') 
+    expect(@cell.ship).to eq(nil)
+    expect(@cell.empty?).to eq(true)
   end
 
   it 'can place a ship on the cell' do
