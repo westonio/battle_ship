a13a88bc33b6966e9844456f8554d94b586ab329
Weston Schutt <wtschutt@gmail.com>
2023-05-26 14:50:59 -0600

    Feature: add initialize and create_cells methods

--- lib/board.rb
+++ lib/board.rb
@@ -1,3 +1,17 @@
 class Board
-  
+  attr_reader :cells
+
+  def initialize
+    @cells = create_cells
+  end
+
+
+  def create_cells
+    keys = ["A1", "A2", "A3", "A4", "B1", "B2", "B3", "B4", "C1", "C2", "C3", "C4", "D1", "D2", "D3", "D4"]
+    cells = {}
+    keys.map do |key|
+      cells[key] = Cell.new(key)
+    end
+    cells
+  end
 end
