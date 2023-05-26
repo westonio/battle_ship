84b90aebdd7f2efe56ea9801dde80ea1e9c202a1
Connor Richmond <richmond.connor@gmail.com>
2023-05-26 16:50:48 -0400

    Add tests for coordinate validation

--- lib/board.rb
+++ lib/board.rb
@@ -1,3 +1,19 @@
 class Board
-  
+
+  attr_reader :cells
+
+  def initialize
+    @cells = create_cells
+  end
+
+  def create_cells
+    cells = {}
+    ('A'..'D').each do |letter|
+      (1..4).each do |number|
+        cell_key = "#{letter}#{number}"
+        cells[cell_key] = Cell.new
+      end
+    end
+    cells
+  end
 end
