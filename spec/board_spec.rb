9550c8464a40cc2e3481a57f7439325a6004923f
Weston Schutt <wtschutt@gmail.com>
2023-05-26 14:58:21 -0600

    Update .valid_coordinate? method tests

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -17,4 +17,14 @@ RSpec.describe Board do
     expect(@board.cells).to be_a(Hash)
     expect(@board.cells.length).to eq(16)
   end
+
+  it 'checks if coordinate is valid' do
+    expect(@board.valid_coordinate?("A1")).to be true
+    expect(@board.valid_coordinate?("D4")).to be true
+
+    expect(@board.valid_coordinate?("A5")).to be false
+    expect(@board.valid_coordinate?("E1")).to be false 
+    expect(@board.valid_coordinate?("A22")).to be false
+  end
+
 end
