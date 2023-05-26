297cd4bb78001f596640b44b49172baa04c55c60
Weston Schutt <wtschutt@gmail.com>
2023-05-26 15:01:39 -0600

    "Update back to original"

--- lib/board.rb
+++ lib/board.rb
@@ -1,19 +1,21 @@
 class Board
-
   attr_reader :cells
 
   def initialize
     @cells = create_cells
   end
 
+
   def create_cells
+    keys = ["A1", "A2", "A3", "A4", "B1", "B2", "B3", "B4", "C1", "C2", "C3", "C4", "D1", "D2", "D3", "D4"]
     cells = {}
-    ('A'..'D').each do |letter|
-      (1..4).each do |number|
-        cell_key = "#{letter}#{number}"
-        cells[cell_key] = Cell.new
-      end
+    keys.map do |key|
+      cells[key] = Cell.new(key)
     end
     cells
+  end
+
+  def valid_coordinate?(coordinate)
+    cells.has_key?(coordinate)
   end
 end
