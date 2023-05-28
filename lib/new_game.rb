9dd49a552865875db3bc8eb4dc8f238bfe7c4a76
Connor Richmond <richmond.connor@gmail.com>
2023-05-28 18:02:45 -0400

    Addition to place ship

--- lib/new_game.rb
+++ lib/new_game.rb
@@ -9,14 +9,41 @@ class NewGame
 
   def start
     if @response == 'p'
-      computer_board = Board.new
-      player_board = Board.new
+      play
     elsif @response == 'q'
       puts "Thank you for playing BATTLESHIP!"
     else 
       puts 'Invalid Input Please use either p or q.'
       main_menu
     end
+  end
+
+  def play
+    computer_board = Board.new
+    player_board = Board.new
+
+    # computer random placement goes here
+    player_place_ships(player_board)
+  end
+
+  def player_place_ships
+    puts "I have laid out my ships on the grid. \n" +
+         "You now need to lay out your two ships. \n" +
+         "The Cruiser is three units long and the Submarine is two units long."
+    puts "  1 2 3 4\n" +
+         "A . . . .\n" +
+         "B . . . .\n" +
+         "C . . . .\n" +
+         "D . . . ."
+
+    place_ship("Cruiser")
+  end
+
+  def place_ship(ship, for_cells)
+    puts "Enter the squares for the #{ship})"
+    placement = gets.chomp.split
+
+
   end
 
 end
