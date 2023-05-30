ce0a749a09783ab6c11f7a29ccd67fef150967c6
Weston Schutt <wtschutt@gmail.com>
2023-05-30 16:30:40 -0600

    Update reveal methods to be dynamic for board size

--- lib/board.rb
+++ lib/board.rb
@@ -105,21 +105,38 @@ class Board
     end
   end
 
-  def render(reveal_board = false)
-    if reveal_board 
-      "  1 2 3 4 \n" +
-      "A #{cells["A1"].render(true)} #{cells["A2"].render(true)} #{cells["A3"].render(true)} #{cells["A4"].render(true)} \n" +
-      "B #{cells["B1"].render(true)} #{cells["B2"].render(true)} #{cells["B3"].render(true)} #{cells["B4"].render(true)} \n" +
-      "C #{cells["C1"].render(true)} #{cells["C2"].render(true)} #{cells["C3"].render(true)} #{cells["C4"].render(true)} \n" +
-      "D #{cells["D1"].render(true)} #{cells["D2"].render(true)} #{cells["D3"].render(true)} #{cells["D4"].render(true)} \n"
-
+  def render(reveal_board = false) 
+    if reveal_board
+      "#{line_1} \n" +
+      "#{other_lines(reveal_board)}"
     else
-      "  1 2 3 4 \n" +
-      "A #{cells["A1"].render} #{cells["A2"].render} #{cells["A3"].render} #{cells["A4"].render} \n" +
-      "B #{cells["B1"].render} #{cells["B2"].render} #{cells["B3"].render} #{cells["B4"].render} \n" +
-      "C #{cells["C1"].render} #{cells["C2"].render} #{cells["C3"].render} #{cells["C4"].render} \n" +
-      "D #{cells["D1"].render} #{cells["D2"].render} #{cells["D3"].render} #{cells["D4"].render} \n"
+      "#{line_1} \n" +
+      "#{other_lines(reveal_board)}"
     end
+  end
+
+  def line_1
+    string = " "
+    (1..size).each do |num|
+      string += " #{num}"
+    end
+    string
+  end
+
+  def other_lines(reveal_board)
+    letters = ("A"..last_letter).to_a
+    string = ""
+    letters.each do |letter|
+      string += "#{letter}"
+      letter_keys = create_keys.select do |key|
+        key[0] == letter
+      end
+      letter_keys.map do |key|
+        string += " #{cells[key].render(reveal_board)}"
+      end 
+      string += " \n"
+    end
+    string
   end
 
   def randomly_place(ship)
